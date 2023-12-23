9a9ef80ff7d75b7ec70adaaa43bbfc821b989fa0
Bryan Dellinger <bryan.dellinger.civ@armwarcollege.edu>
2023-12-22 20:23:43 -0500

    fixing azure

--- client-app/src/app/layout/ManageRegistrationNavbar.tsx
+++ client-app/src/app/layout/ManageRegistrationNavbar.tsx
@@ -24,60 +24,56 @@ export default observer(function ManageRegistrationNavbar() {
     }, []);
 
     return (
-        <>
-            {isMobile ? (
-              <div style={{ width: '100%', backgroundColor: 'grey' }}>
-              <Menu inverted color='grey' style={{ width: '100%', display: 'flex' }}>
-                  <Menu.Item onClick={() => setSidebarVisible(true)} style={{ flexShrink: 0 }}>
-                      <Icon name="sidebar" />
-                  </Menu.Item>
-                  <Menu.Item style={{ flexGrow: 1, justifyContent: 'center', display: 'flex' }}>
-                      <ArmyLogo content={'REGISTRATION PORTAL'} size="1em" textColor="#FFF" outerStarColor="yellow" innerStarColor="grey" />
-                  </Menu.Item>
-                  <Menu.Item style={{ flexShrink: 0 }}>
-                      <Dropdown 
-                          trigger={<><Icon name="user" /> {user?.displayName}</>} 
-                          pointing="top right">
-                          <Dropdown.Menu>
-                              <Dropdown.Item icon="power" text="Logout" onClick={logout}/>
-                          </Dropdown.Menu>
-                      </Dropdown>
-                  </Menu.Item>
-              </Menu>
-              <Sidebar
-                  as={Menu}
-                  animation='overlay'
-                  icon='labeled'
-                  inverted
-                  onHide={() => setSidebarVisible(false)}
-                  vertical
-                  visible={sidebarVisible}
-                  width='thin'
-              >
-                  <Menu.Item as={NavLink} to={'/'}>Home</Menu.Item>
-                  <Menu.Item as={NavLink} to={'/myregistrations'}>My Registrations</Menu.Item>
-                  {/* Add other menu items here */}
-              </Sidebar>
-          </div>
-            ) : (
-                <div style={{ width: '100%', backgroundColor: 'grey' }}>
-                    <Menu inverted color='grey' style={{ width: '100%' }}>
-                        <Menu.Item as={NavLink} to={'/'}>
-                            <ArmyLogo content={'REGISTRATION PORTAL'} size="1.7em" textColor="#FFF" outerStarColor="yellow" innerStarColor="grey" />
-                        </Menu.Item>
-                        <Menu.Item name='My Registrations' active/>
-                        <Menu.Item position="right">
-                            <Dropdown 
-                                trigger={<><Icon name="user" /> {user?.displayName}</>} 
-                                pointing="top right">
-                                <Dropdown.Menu>
-                                    <Dropdown.Item icon="power" text="Logout" onClick={logout}/>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Menu.Item>
-                    </Menu>
-                </div>
-            )}
-        </>
+      <>
+      {isMobile ? (
+        <div style={{ width: '100%', backgroundColor: 'grey' }}>
+          <Menu inverted color='grey' style={{ width: '100%', display: 'flex' }}>
+            <Menu.Item onClick={() => setSidebarVisible(true)} style={{ flexShrink: 0 }}>
+              <Icon name="sidebar" />
+            </Menu.Item>
+            <Menu.Item style={{ flexGrow: 1, justifyContent: 'center', display: 'flex' }}>
+              <ArmyLogo content='REGISTRATION PORTAL' size="1em" textColor="#FFF" outerStarColor="yellow" innerStarColor="grey" />
+            </Menu.Item>
+            <Menu.Item style={{ flexShrink: 0 }}>
+              <Dropdown trigger={<><Icon name="user" /> {user?.displayName}</>}>
+                <Dropdown.Menu>
+                  <Dropdown.Item icon="power" text="Logout" onClick={logout} />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+          </Menu>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={() => setSidebarVisible(false)}
+            vertical
+            visible={sidebarVisible}
+            width='thin'
+          >
+            <Menu.Item as={NavLink} to='/'>Home</Menu.Item>
+            <Menu.Item as={NavLink} to='/myregistrations'>My Registrations</Menu.Item>
+            {/* Add other menu items here */}
+          </Sidebar>
+        </div>
+      ) : (
+        <div style={{ width: '100%', backgroundColor: 'grey' }}>
+          <Menu inverted color='grey' style={{ width: '100%' }}>
+            <Menu.Item as={NavLink} to='/'>
+              <ArmyLogo content='REGISTRATION PORTAL' size="1.7em" textColor="#FFF" outerStarColor="yellow" innerStarColor="grey" />
+            </Menu.Item>
+            <Menu.Item name='My Registrations' active />
+            <Menu.Item position="right">
+              <Dropdown trigger={<><Icon name="user" /> {user?.displayName}</>}>
+                <Dropdown.Menu>
+                  <Dropdown.Item icon="power" text="Logout" onClick={logout} />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+          </Menu>
+        </div>
+      )}
+    </>
     );
 });
