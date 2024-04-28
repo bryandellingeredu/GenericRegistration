7d69ba000fdeab936641f96371f0201f0e542042
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2024-04-28 15:02:14 -0400

    instructions

---
+++ API/Controllers/DocumentUploadWebsites.cs
@@ -0,0 +1,20 @@
+﻿using Application.DocumentUploadWebsites;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class DocumentUploadWebsitesController : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpGet("{registrationEventId}")]
+        public async Task<ActionResult> GetRegistration(Guid registrationEventId) =>
+            HandleResult(await Mediator.Send(new Details.Query { RegistrationEventId = registrationEventId }));
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRegistration(DocumentUploadWebsite documentUploadWebsite) =>
+           HandleResult(await Mediator.Send(new CreateUpdate.Command{ DocumentUploadWebsite = documentUploadWebsite }));
+    }
+}
+
