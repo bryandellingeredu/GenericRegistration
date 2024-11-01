6c6e6daa16e990cc5a7a9aacd972b37a5d159ae8
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2024-11-01 08:26:39 -0400

    adding send email api

---
+++ API/Controllers/SendEmail.cs
@@ -0,0 +1,30 @@
+﻿
+using Application.SendEmail;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class SendEmail : BaseApiController
+    {
+        private readonly IConfiguration _configuration;
+
+        public SendEmail(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> EmailLink([FromBody] EmailRequestDTO emailRequestDTO, [FromHeader(Name = "X-API-KEY")] string apiKey)
+        {
+            var configuredApiKey = _configuration["SecuritySettings:ApiKey"];
+            if (string.IsNullOrEmpty(apiKey) || apiKey != configuredApiKey)
+            {
+                return Unauthorized("Invalid API Key.");
+            }
+
+            return HandleResult(await Mediator.Send(new Create.Command { EmailRequestDTO = emailRequestDTO }));
+        }
+    }
+}
---
+++ Application/SendEmail/Create.cs
@@ -0,0 +1,50 @@
+﻿using Application.Core;
+using Application.EmailLink;
+using Domain;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.SendEmail
+{
+    public class Create
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public EmailRequestDTO EmailRequestDTO { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly IConfiguration _config;
+
+            public Handler(IConfiguration config) {
+                _config = config;   
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                Settings s = new Settings();
+                var settings = s.LoadSettings(_config);
+                GraphHelper.InitializeGraph(settings, (info, cancel) => Task.FromResult(0));
+                try
+                {
+                    await GraphHelper.SendEmail(
+                        request.EmailRequestDTO.Recipients,request.EmailRequestDTO.Subject, request.EmailRequestDTO.Body);
+
+                    return Result<Unit>.Success(Unit.Value);
+                }
+                catch (Exception ex)
+                {
+
+                    return Result<Unit>.Failure($"An error occurred when trying to send email: {ex.Message}");
+                }
+            }
+        }
+    }
+}
